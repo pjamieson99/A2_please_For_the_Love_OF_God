af24a24d6909e4d88091f35bc880f2f74e712fa2
pjamieson99 <32102111+pjamieson99@users.noreply.github.com>
2017-12-20 12:01:52 +0000

    Add files via upload
    datatbase doesnt work properly fix this then go on and start epq

--- A2_project_Clean/A2_project_Clean/CJoint.vb
+++ A2_project_Clean/A2_project_Clean/CJoint.vb
@@ -4,6 +4,10 @@
     Private diameter As Integer
     Public JYpos As Integer
     Public JXpos As Integer
+    Dim DrawX As Integer
+
+    Public PrevJoint As CJoint
+
 
     'set properties
     Public Sub New(x As Integer, y As Integer, w As Integer, d As Integer)
@@ -13,10 +17,20 @@
         JYpos = y
         JXpos = x
 
+
     End Sub
 
+
+    Function Clone()
+        Return Me.MemberwiseClone()
+    End Function
+
+    Sub PrevPoint()
+        PrevJoint = New CJoint(JXpos, JYpos, width, diameter)
+    End Sub
     'draw
     Public Sub Draw(g As Graphics, ColourPen As Pen)
+        '    DrawX = JXpos - Form1.FurthestAnimalPos
 
         g.FillEllipse(New SolidBrush(ColourPen.Color), JXpos - 5, JYpos - 5, width, diameter)
     End Sub
